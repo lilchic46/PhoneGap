ec1b24d004c6ed4467a0b4f113908115788690e0
lilchic46 <77950709+lilchic46@users.noreply.github.com>
2021-11-21 15:04:43 +0700

    Blank changes

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,9 @@
-
 document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {}
 
 $(document).on('vclick', '#btn-cordova-confirm', cordovaConfirm);
 $(document).on('vclick', '#btn-cordova-beep', cordovaBeep);
 $(document).on('vclick', '#btn-cordova-vibration', cordovaVibration);
-
-// Installation:
-// cordova plugin add cordova-plugin-dialogs
 
 function cordovaConfirm() {
     var message = 'Please choose your action.';
@@ -17,38 +13,25 @@ function cordovaConfirm() {
     navigator.notification.confirm(message, callback, title, buttonLabel);
 
     function callback(btnIndex) {
-        //alert('You can run codes here.');
-
         if (btnIndex == 0) {
-            alert('You have not choosen any button.')
-            
+            alert('You have not choosen any button.')            
         }
 
         else if (btnIndex == 1) {
             alert('You choose to ring a bell.');
-            navigator.notification.beep(4);
-            
+            navigator.notification.beep(4);            
         }
 
         else if (btnIndex == 2) {
             alert('You choose to vibrate the mobile.');
             navigator.vibrate(400, 600, 1000, 400, 600, 1000);
         }
-
-        //alert('And here...');
     }
 }
 
 function cordovaBeep() {
     navigator.notification.beep(4);
 }
-
-// VIBRATION
-// https://cordova.apache.org/docs/en/10.x/reference/cordova-plugin-vibration/index.html
-
-// Installation:
-// cordova plugin add cordova-plugin-vibration
-
 
 function cordovaVibration() {
     navigator.vibrate(1000, 1000, 1000, 3000, 3000, 3000, 1000, 1000, 1000);
